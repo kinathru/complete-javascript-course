b48be7822626316632800ecc5ea1c2cd68110bda
kinath <kinath.ru@gmail.com>
2026-04-05 13:15:49 -0500

    feat:handling styles and attributes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -56,15 +56,57 @@ message.classList.add('cookie-message');
 message.textContent = 'We use cookies for improve functionality and analytics';
 message.innerHTML =
   'We use cookies for improve functionality and analytics ' +
-  ' <button class="btn btn--colse-cookie">Got it!</button>';
+  ' <button class="btn btn--close-cookie">Got it!</button>';
 // header.prepend(message);
 // header.append(message.cloneNode(true));
-header.before(message);
-// header.after(message);
+// header.before(message);
+header.after(message);
 
 document
-  .querySelector('btn--close--cookie')
+  .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
-    // message.remove();
-    message.parentElement.removeChild(message);
+    message.remove();
+    // message.parentElement.removeChild(message);
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+console.log(message.style.height); // Note that we don't get anything, becuase it's not set as an inline style
+console.log(message.style.backgroundColor); // Here we get a value because, it's available as an inline style
+
+console.log(getComputedStyle(message));
+console.log(getComputedStyle(message).height);
+console.log(getComputedStyle(message).color);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.designer);
+
+logo.alt = 'Beautiful minimalist logo';
+
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('');
+logo.classList.remove('');
+logo.classList.toggle('');
+logo.classList.contains('');
+
+// Don't use
+logo.className = 'jonas';
